1473f1eaf9e4880b365dd4e2806835f8c7a42b3e
xiaochenghua <chenghuaxiao.xch@alibaba-inc.com>
2022-11-18 21:28:43 +0800

    feat: 广告

--- pages/index/index.js
+++ pages/index/index.js
@@ -14,6 +14,25 @@ Page({
 
   onLoad() {
     this.echart =this.selectComponent('#mychart');
+    // 在页面中定义插屏广告
+let interstitialAd = null
+
+// 在页面onLoad回调事件中创建插屏广告实例
+if (wx.createInterstitialAd) {
+  interstitialAd = wx.createInterstitialAd({
+    adUnitId: 'adunit-a4fab2c7a6cb6510'
+  })
+  interstitialAd.onLoad(() => {})
+  interstitialAd.onError((err) => {})
+  interstitialAd.onClose(() => {})
+}
+
+// 在适合的场景显示插屏广告
+if (interstitialAd) {
+  interstitialAd.show().catch((err) => {
+    console.error(err)
+  })
+}
   },
 
   handleDate(e) {
